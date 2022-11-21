a8aa18bfc627f39ccdf740e7c7e3b9cda83d8713
albuquerque-lucas <lucaslpra@gmail.com>
2022-11-21 20:32:58 -0300

    Alterando referência para negociacoes em negociacoes.ts

--- app/controllers/negociacao-controller.ts
+++ app/controllers/negociacao-controller.ts
@@ -1,23 +1,30 @@
 import { Negociacao } from '../models/negociacao.js';
+import { Negociacoes } from '../models/negociacoes.js';
 
 export class NegociacaoController {
+
     private inputData: HTMLInputElement;
     private inputQuantidade: HTMLInputElement;
     private inputValor: HTMLInputElement;
+    private negociacoes = new Negociacoes();
 
-    constructor() {
+    constructor()
+    {
         this.inputData = document.querySelector('#data');
         this.inputQuantidade = document.querySelector('#quantidade');
         this.inputValor = document.querySelector('#valor');
     }
 
-    adiciona(): void {
+    adiciona(): void
+    {
         const negociacao = this.criaNegociacao();
-        console.log(negociacao);
+        this.negociacoes.adiciona(negociacao);
+        console.log(this.negociacoes.lista());
         this.limparFormulario();
     }
 
-    criaNegociacao(): Negociacao {
+    criaNegociacao(): Negociacao
+    {
         const exp = /-/g;
         const date = new Date(this.inputData.value.replace(exp, ','));
         const quantidade = parseInt(this.inputQuantidade.value);
@@ -25,7 +32,8 @@ export class NegociacaoController {
         return new Negociacao(date, quantidade, valor);
     }
 
-    limparFormulario(): void {
+    limparFormulario(): void
+    {
         this.inputData.value = '';
         this.inputQuantidade.value = '';
         this.inputValor.value = '';
