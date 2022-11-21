360cfa92e7cf179b08557d0846f6bec41802d08e
albuquerque-lucas <lucaslpra@gmail.com>
2022-11-21 18:12:41 -0300

    Continunando alterações para adaptar para typescript

--- app/controllers/negociacao-controller.ts
+++ app/controllers/negociacao-controller.ts
@@ -7,23 +7,31 @@ export class NegociacaoController {
     private inputValor: HTMLInputElement;
 
 
-    constructor(){
+    constructor()
+    {
         this.inputData = document.querySelector('#data');
         this.inputQuantidade = document.querySelector('#quantidade');
         this.inputValor = document.querySelector('#valor');
     }
 
-    adiciona(){
+    adiciona(): void
+    {
+
+        const negociacao = this.criaNegociacao();
+
+        console.log(negociacao);
+        
+    }
+
+    criaNegociacao(): Negociacao
+    {
 
         const exp = /-/g;
         const date = new Date(this.inputData.value.replace(exp, ','));
         const quantidade = parseInt(this.inputQuantidade.value);
         const valor = parseFloat(this.inputValor.value);
 
-        const negociacao = new Negociacao(date, quantidade, valor);
-
-        console.log(negociacao);
-        
+        return new Negociacao(date, quantidade, valor);
     }
 
 
