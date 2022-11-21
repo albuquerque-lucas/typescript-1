9881c45f8a271717d1f80287e460e85db3738e09
albuquerque-lucas <lucaslpra@gmail.com>
2022-11-21 19:21:38 -0300

    Consertando a estruturação de arquivos

--- app/models/negociacao.ts
+++ app/models/negociacao.ts
@@ -1,28 +1,27 @@
 export class Negociacao {
-
+    private _data: Date;
     private _quantidade: number;
     private _valor: number;
-    private _data: Date;
 
-    constructor(data: Date, quantidade: number, valor: number){
+    constructor(data: Date, quantidade: number, valor: number) {
         this._data = data;
         this._quantidade = quantidade;
         this._valor = valor;
     }
 
-    get data(): Date
-    {
+    get data(): Date {
         return this._data;
     }
 
-    get quantidade(): number
-    {
+    get quantidade(): number {
         return this._quantidade;
     }
 
-    get valor(): number
-    {
+    get valor(): number {
         return this._valor;
     }
 
+    get volume(): number {
+        return this._quantidade * this._valor;
+    }
 }
